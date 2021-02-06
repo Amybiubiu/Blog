d10b00753a7192e7dce66bea17dc88823229e22d
Amybiubiu <1246457100@qq.com>
2021-02-06 22:15:06 +0800

    My don't know bind 555

--- js/bind.js
+++ js/bind.js
@@ -1,11 +1,21 @@
 // 第二版
 Function.prototype.bind2 = function (context) {
+    // 用 this 获取调用 bind 的函数
     var self = this;
-    var args = Array.prototype.slice.apply(this, arguments, 1);
-    return function(){
-        var bindArgs = Array.prototype.slice.apply(this, arguments);
-        self.apply(this, context, args.concat(bindArgs));
+    // 获取bind2函数从第二个参数到最后一个参数
+    var args = Array.prototype.slice.apply(arguments, 1);
+    var fBound = function(){
+        // 这个时候的arguments是指bind返回的函数传入的参数
+        var bindArgs = Array.prototype.slice.apply(arguments);
+        // 当作为构造函数时，this 指向实例，此时结果为 true，将绑定函数的 this 指向该实例，可以让实例获得来自绑定函数的值
+        // 以上面的是 demo 为例，如果改成 `this instanceof fBound ? null : context`，实例只是一个空对象，将 null 改成 this ，实例会具有 habit 属性
+        // 当作为普通函数时，this 指向 window，此时结果为 false，将绑定函数的 this 指向 context
+        return self.apply(this instanceof fBound ? this : context, args.concat(bindArgs));
     }
+
+    fBound._proto_ = this.prototype;
+
+    return fBound;
 }
 
 var value = 2;
